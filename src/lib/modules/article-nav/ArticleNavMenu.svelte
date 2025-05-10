84fcf1342bbac32f44dc30d3cd150e87ab33fb7c
Daniil Shilo <tokiory.personal@gmail.com>
2025-05-10 19:43:42 +0300

    feat(articles): add navigation mark

--- src/lib/modules/article-nav/ArticleNavMenu.svelte
+++ src/lib/modules/article-nav/ArticleNavMenu.svelte
@@ -14,40 +14,59 @@
 	const handleClick = (e: MouseEvent) => {
 		if (e.target === e.currentTarget) onclose();
 	};
+
+	const links = [
+		{
+			title: 'Домой',
+			uri: '/',
+			icon: 'i-tabler-home'
+		},
+		{
+			title: 'Статьи',
+			uri: '/articles',
+			icon: 'i-tabler-note'
+		}
+	];
 </script>
 
 <!-- eslint-disable-next-line svelte/no-unused-svelte-ignore -->
 <!-- svelte-ignore a11y_no_static_element_interactions,a11y_click_events_have_key_events -->
-<div aria-label="backdrop of navigation" onclick={handleClick} class="menu h-screen p-2 fixed py-24 inset-0 z-20">
+<div
+	aria-label="backdrop of navigation"
+	onclick={handleClick}
+	class="menu h-screen px-4 fixed py-24 inset-0 z-20"
+>
 	<div class="mx-auto h-full flex flex-col max-w-[854px]">
 		<div class="flex justify-between items-center">
 			<ContentH2 nospy class="!mt-0">Оглавление</ContentH2>
 			<div class="flex gap-2">
-				<a
-					href="/"
-					class="border-b-2 border-dashed border-driftwood-800/30 flex gap-1 items-center"
-				>
-					<div class="i-tabler-home"></div>
-					Домой
-				</a>
-				<a
-					href="/articles"
-					class="border-b-2 border-dashed border-driftwood-800/30 flex gap-1 items-center"
-				>
-					<div class="i-tabler-note"></div>
-					Статьи
-				</a>
+				{#each links as link (link)}
+					<a
+						href={link.uri}
+						class="border-b-2 hover:border-driftwood-800/50 active:border-driftwood-800/70 border-dashed border-driftwood-800/30 flex gap-1 items-center"
+					>
+						<div class={link.icon}></div>
+						{link.title}
+					</a>
+				{/each}
 			</div>
 		</div>
-		<ul class="navigation overflow-y-auto mt-3 overflow-y-auto">
+		<ul class="navigation mt-3 overflow-y-auto">
 			{#each headings as heading, idx (idx)}
 				<li
-					class={[heading.level == 2 && 'ml-4', heading.level == 3 && 'ml-8']}
-					class:active={idx === currentHeadingId}
+					class={[
+						'flex items-center justify-between pr-2 gap-1',
+						heading.level == 2 && 'ml-4',
+						heading.level == 3 && 'ml-8',
+						idx === currentHeadingId && 'font-bold'
+					]}
 				>
 					<button aria-label="выбрать пункт навигации" onclick={() => onselect(idx)}>
 						{heading.text}
 					</button>
+					{#if idx === currentHeadingId}
+					<div class="i-tabler-arrow-left"></div>
+					{/if}
 				</li>
 			{/each}
 		</ul>
