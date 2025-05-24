391aa79289946e4c2a335d0cb4769e589a549ed5
Daniil Shilo <tokiory.personal@gmail.com>
2025-05-24 18:43:38 +0300

    feat(error): add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<div class="h-screen w-full flex flex-col items-center justify-center">
+	<object class="h-220px" type="image/svg+xml" data="/illustrations/dragon.svg"></object>
+	<div class="text-center">
+		<h1 class="text-3xl font-bold">
+			{#if page.status === 404}
+				Страница не найдена
+			{:else}
+				Что-то пошло не так...
+			{/if}
+		</h1>
+		<a href="/" class="mt-8 block">
+			<button class="py-1.5 p-1 border-2 border-dashed rounded-md border-driftwood-800/30 hover:border-driftwood-800/50 transition-colors px-6">На главную</button>
+		</a>
+	</div>
+</div>
