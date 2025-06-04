d92db23d4128b1045f16c33e7e04b4f0f962c843
Daniil Shilo <tokiory.personal@gmail.com>
2025-06-05 00:24:23 +0300

    feat(header): add articles link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -11,9 +11,20 @@
 
 <header class={['flex items-center justify-between', className]}>
 	<Logo />
-	<ul class="flex gap-3 items-center">
-		<li><Link target="_blank" href={TELEGRAM_LINK} darker>Телеграм</Link></li>
-		<li><Link target="_blank" href={GITHUB_LINK} darker>Гитхаб</Link></li>
-		<li><Link target="_blank" href={HABR_LINK} darker>Хабр</Link></li>
-	</ul>
+	<div class="flex flex-col gap-1">
+		<ul class="flex gap-3 justify-end items-center">
+			<li>
+				<Link class="relative" darker target="_blank" href="/articles">
+					Статьи
+					<div class="outer-icon absolute -top-0.75 -right-3.5 i-tabler-arrow-up-right text-xs"></div>
+				</Link>
+			</li>
+		</ul>
+		<div class="border-b-1 border-dashed border-driftwood-800/70"></div>
+		<ul class="flex text-sm gap-3 justify-end items-center">
+			<li><Link darker target="_blank" href={TELEGRAM_LINK}>Телеграм</Link></li>
+			<li><Link darker target="_blank" href={GITHUB_LINK}>Гитхаб</Link></li>
+			<li><Link darker target="_blank" href={HABR_LINK}>Хабр</Link></li>
+		</ul>
+	</div>
 </header>
