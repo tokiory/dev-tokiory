d4646f8d3c5a58b610cd0b4272273f906943444f
Daniil Shilo <tokiory.personal@gmail.com>
2025-06-05 03:03:09 +0300

    fix(header): some semantic changes

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,22 +13,26 @@
 <header class={['pr-2 xl:pr-0 flex items-center justify-between', className]}>
 	<Logo />
 	<div class="hidden sm:flex flex-col gap-1">
-		<ul class="flex gap-3 justify-end items-center">
-			<li>
-				<Link class="relative" darker target="_blank" href="/articles">
-					Статьи
-					<div
-						class="outer-icon absolute -top-0.75 -right-3.5 i-tabler-arrow-up-right text-xs"
-					></div>
-				</Link>
-			</li>
-		</ul>
+		<nav>
+			<ul class="flex gap-3 justify-end items-center">
+				<li>
+					<Link class="relative" darker href="/articles">
+						Статьи
+						<div
+							class="outer-icon absolute -top-0.75 -right-3.5 i-tabler-arrow-up-right text-xs"
+						></div>
+					</Link>
+				</li>
+			</ul>
+		</nav>
 		<div class="border-b-1 border-dashed border-driftwood-800/70"></div>
-		<ul class="flex text-sm gap-3 justify-end items-center">
-			<li><Link darker target="_blank" href={TELEGRAM_LINK}>Телеграм</Link></li>
-			<li><Link darker target="_blank" href={GITHUB_LINK}>Гитхаб</Link></li>
-			<li><Link darker target="_blank" href={HABR_LINK}>Хабр</Link></li>
-		</ul>
+		<aside>
+			<ul class="flex text-sm gap-3 justify-end items-center">
+				<li><Link darker target="_blank" href={TELEGRAM_LINK}>Телеграм</Link></li>
+				<li><Link darker target="_blank" href={GITHUB_LINK}>Гитхаб</Link></li>
+				<li><Link darker target="_blank" href={HABR_LINK}>Хабр</Link></li>
+			</ul>
+		</aside>
 	</div>
 	<button aria-label="Menu" onclick={onmenu} class="py-1 pl-3 sm:hidden text-xl cursor-pointer">
 		<div class="i-tabler-menu-2"></div>
