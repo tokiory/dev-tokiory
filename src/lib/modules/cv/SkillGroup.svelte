3c823debd8522dea7a2dc86a186fb46384b128a3
Daniil Shilo <tokiory.personal@gmail.com>
2025-06-28 00:11:55 +0300

    chore(skills): add eslint disable comment for SkillGroup

--- src/lib/modules/cv/SkillGroup.svelte
+++ src/lib/modules/cv/SkillGroup.svelte
@@ -8,8 +8,14 @@
 <div class="border-b-2 pb-0.5 border-driftwood-800/70 border-dashed">
 	<span class="text-driftwood-700 font-semibold">{category}:</span>
 	{#each skills as skill, idx (skill.name + skill.link)}
-		<a class="hover:text-driftwood-900 transition-colors" href={skill.link} target="_blank" rel="noopener noreferrer">
+		<a
+			class="hover:text-driftwood-900 transition-colors"
+			href={skill.link}
+			target="_blank"
+			rel="noopener noreferrer"
+		>
 			{skill.name}
+			<!-- eslint-disable-next-line svelte/no-useless-mustaches -->
 		</a>{#if idx < skills.length - 1}<span>{', '}</span>{/if}
 	{/each}
 </div>
