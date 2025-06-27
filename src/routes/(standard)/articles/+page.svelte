b02f1dd69dffa75dff55f8fb6eeabf7520284b5a
Daniil Shilo <tokiory.personal@gmail.com>
2025-06-28 02:19:13 +0300

    feat(articles): add search empty results stub

--- src/routes/(standard)/articles/+page.svelte
+++ src/routes/(standard)/articles/+page.svelte
@@ -12,9 +12,10 @@
 	let posts = $derived(filteredPosts || data.posts);
 	let query = $state('');
 	let postsRef: HTMLElement[] = $state([]);
+	let notFoundRef: HTMLElement | null = $state(null);
 
 	$effect(() => {
-		animate(postsRef, {
+		animate([...postsRef, notFoundRef], {
 			...fadeUp,
 			delay: (el, i) => i * 100
 		});
@@ -33,18 +34,22 @@
 
 	$effect(() => {
 		const normalizedQuery = query.toLowerCase().trim();
-		refetchPosts(normalizedQuery)
+		refetchPosts(normalizedQuery);
 	});
 </script>
 
 <main class="mt-10">
 	<div class="text-xs text-driftwood-900/50">Поиск</div>
 	<Input bind:value={query} class="w-full" placeholder="Введите что-то..." />
-	<div class="flex items-center gap-8 flex-wrap mt-4">
-		{#each posts as post, index (post.slug + index)}
-			<div class="opacity-0 w-full" bind:this={postsRef[index]}>
-				<ListingArticle {...post}>{post.title}</ListingArticle>
-			</div>
-		{/each}
-	</div>
+	{#if filteredPosts != null && filteredPosts.length === 0}
+		<div bind:this={notFoundRef} class="text-center text-driftwood-900/70 mt-8">Ничего не найдено</div>
+	{:else}
+		<div class="flex items-center gap-8 flex-wrap mt-4">
+			{#each posts as post, index (post.slug + index)}
+				<div class="opacity-0 w-full" bind:this={postsRef[index]}>
+					<ListingArticle {...post}>{post.title}</ListingArticle>
+				</div>
+			{/each}
+		</div>
+	{/if}
 </main>
