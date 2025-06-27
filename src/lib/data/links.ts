3e67cd2ef0d96a2fdd8989560091b16944e58586
Daniil Shilo <tokiory.personal@gmail.com>
2025-06-27 19:25:20 +0300

    feat(links): bring back old habr link

--- src/lib/data/links.ts
+++ src/lib/data/links.ts
@@ -1,3 +1,5 @@
 export const TELEGRAM_LINK = "https://t.me/tokiory"
 export const GITHUB_LINK = "https://github.com/tokiory"
-export const HABR_LINK = "https://habr.com/ru/users/tokiory/"
+// TODO: Fill habr page and bring it back
+// export const HABR_LINK = "https://habr.com/ru/users/tokiory/"
+export const HABR_LINK = "https://habr.com/ru/users/crackidocky/"
