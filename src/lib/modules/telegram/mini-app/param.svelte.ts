c895c9803f9ea4a4ea51af4507504eda1c32a64a
Daniil Shilo <tokiory.personal@gmail.com>
2025-07-18 04:11:22 +0300

    feat(twa): add tracking of article id in twa

--- src/lib/modules/telegram/mini-app/param.svelte.ts
+++ src/lib/modules/telegram/mini-app/param.svelte.ts
@@ -1,11 +1,18 @@
 const enum TelegramParam {
-	StartParam = 'start_param'
+	StartParam = 'tgWebAppStartParam'
 }
 
 export const paramStore = {
 	get article(): string | null {
-		const params = new URLSearchParams(window.location.hash);
+		const params = new URLSearchParams(window.location.hash.slice(1));
 		const startParam = params.get(TelegramParam.StartParam) || '';
-		return startParam.match(/article_([a-zA-Z-]+)/)?.[1] || null;
+		const articleId = startParam.match(/article_([a-zA-Z-]+)/)?.[1];
+
+		if (articleId) {
+			umami.track('tma_article', { articleId });
+			return articleId;
+		}
+
+		return null;
 	}
 };
